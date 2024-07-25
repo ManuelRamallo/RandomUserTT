d9d533c1febc725603dd49c9ec06893c9f00e996
Manuel Ramallo Díaz <32164753+manuelramallo@users.noreply.github.com>
2024-07-25 14:16:20 +0200

    Change name to test git

--- RandomUserTT/app/src/main/java/com/mramallo/randomusertt/MainActivity.kt
+++ RandomUserTT/app/src/main/java/com/mramallo/randomusertt/MainActivity.kt
@@ -21,7 +21,7 @@ class MainActivity : ComponentActivity() {
             RandomUserTTTheme {
                 Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
                     Greeting(
-                        name = "Android",
+                        name = "Manuel Ramallo",
                         modifier = Modifier.padding(innerPadding)
                     )
                 }
