1c3cdbc9fa4f0008d6ac648ef368604a423267e1
Manuel Ramallo <mramallo@ext.laligatech.com>
2024-07-26 15:25:24 +0200

    Feature: Navcontroller clicked added

--- RandomUserTT/app/src/main/java/com/mramallo/randomusertt/ui/randomUser/RandomUserScreen.kt
+++ RandomUserTT/app/src/main/java/com/mramallo/randomusertt/ui/randomUser/RandomUserScreen.kt
@@ -6,29 +6,21 @@ import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.PaddingValues
-import androidx.compose.foundation.layout.Row
-import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.aspectRatio
 import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.fillMaxWidth
-import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.grid.GridCells
 import androidx.compose.foundation.lazy.grid.LazyVerticalGrid
 import androidx.compose.foundation.shape.RoundedCornerShape
-import androidx.compose.material3.Button
 import androidx.compose.material3.Card
 import androidx.compose.material3.CardDefaults
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.draw.shadow
 import androidx.compose.ui.graphics.Brush
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.layout.ContentScale
-import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.text.TextStyle
 import androidx.compose.ui.text.font.FontWeight
@@ -40,7 +32,6 @@ import androidx.navigation.NavController
 import androidx.navigation.compose.rememberNavController
 import androidx.paging.compose.collectAsLazyPagingItems
 import coil.compose.AsyncImage
-import coil.request.ImageRequest
 import com.mramallo.randomusertt.R
 import com.mramallo.randomusertt.core.navigation.AppScreens
 import com.mramallo.randomusertt.ui.randomUserDetail.domain.entity.RandomUserItem
@@ -63,39 +54,22 @@ fun RandomUserScreen(
         ) {
             items(randomUsers.itemCount) {
                 randomUsers[it]?.let { randomUserItem ->
-                    DisplayRandomUser(randomUserItem)
+                    DisplayRandomUser(randomUserItem, navController)
                 }
             }
         }
     }
-
-
-    /*Column(
-        modifier = Modifier.fillMaxSize(),
-        verticalArrangement = Arrangement.Center,
-        horizontalAlignment = Alignment.CenterHorizontally
-    ) {
-        Text(text = "Random user list")
-
-        Spacer(modifier = Modifier.height(Theme.spacings.size20))
-
-        Button(
-            onClick = {
-                navController.navigate(route = AppScreens.RandomUserDetailScreen.route)
-            }
-        ) {
-            Text(text = "Navegar al detalle")
-        }
-    }*/
 }
 
 @Composable
-fun DisplayRandomUser(randomUserItem: RandomUserItem) {
+fun DisplayRandomUser(randomUserItem: RandomUserItem, navController: NavController) {
     Card(
         shape = RoundedCornerShape(16.dp),
         modifier = Modifier
             .aspectRatio(3f / 4f)
-            .clickable { },
+            .clickable {
+                navController.navigate(route = AppScreens.RandomUserDetailScreen.route)
+            },
         elevation = CardDefaults.cardElevation(8.dp)
     ) {
         Box(modifier = Modifier.fillMaxSize()) {
